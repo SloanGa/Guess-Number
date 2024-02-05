072b0ae99bb8b9cbe7a323fa99c9ae7301be7b93
Sloan Gauthier <sloangauthier@gmail.com>
2024-02-05 18:56:45 +0100

    add difficulty level

--- main.js
+++ main.js
@@ -1,53 +1,95 @@
 import "./style.css";
 
-const start = document.getElementById("start");
 const initGame = document.getElementById("initGameContainer");
 const startGame = document.getElementById("startGameContainer");
+const explain = document.querySelector(".explain");
 const inputNumber = document.getElementById("number");
 const submit = document.getElementById("try");
-const responsText = document.getElementById("displayResponsText");
+const responsText = document.getElementById("displayResponsContainer");
 const score = document.getElementById("score");
-const displayRespons = document.querySelector(".dp");
 const restart = document.getElementById("restart");
-let count = 0;
+const levelContainer = document.getElementById("level");
+const levelOne = document.getElementById("level1");
+const levelTwo = document.getElementById("level2");
+const levelThree = document.getElementById("level3");
+const levelFour = document.getElementById("level4");
+
+const levels = [levelOne, levelTwo, levelThree, levelFour];
+
+let count;
+let selectedLevel;
+let startNumber;
+let endNumber;
+let difficult;
 
 const getRandomNumber = (max) => {
   return Math.floor(Math.random() * max);
 };
-let randomNumber = getRandomNumber(501);
-console.log(randomNumber);
+let randomNumber = "";
 
-const isValidNumber = (num) => {
-  return !Number.isNaN(num) && num >= 0 && num <= 500;
+const isValidNumber = (num, min, max) => {
+  return !Number.isNaN(num) && num >= min && num <= max;
 };
 
-start.addEventListener("click", () => {
-  initGame.style.display = "none";
-  startGame.style.display = "block";
+levels.forEach((level) => {
+  level.addEventListener("click", () => {
+    count = 0;
+    selectedLevel = level.id;
+    initGame.style.display = "none";
+    levelContainer.style.display = "none";
+    startGame.style.display = "block";
+
+    switch (selectedLevel) {
+      case "level1":
+        startNumber = 1;
+        endNumber = 50;
+        difficult = "Facile";
+        randomNumber = getRandomNumber(51);
+        // console.log(randomNumber);
+        break;
+      case "level2":
+        startNumber = 1;
+        endNumber = 200;
+        difficult = "Intermediaire";
+        randomNumber = getRandomNumber(201);
+        // console.log(randomNumber);
+        break;
+      case "level3":
+        startNumber = 1;
+        endNumber = 500;
+        randomNumber = getRandomNumber(501);
+        difficult = "Difficile";
+        // console.log(randomNumber);
+        break;
+      case "level4":
+        startNumber = 1;
+        endNumber = 1000;
+        difficult = "Cauchemardesque";
+        randomNumber = getRandomNumber(1001);
+        // console.log(randomNumber);
+        break;
+      default:
+        return;
+    }
+
+    explain.innerHTML = `<p>J'ai genéré un nombre entre ${startNumber} et ${endNumber}. Essaye de le trouver.</p>
+    <strong>Niveau : ${difficult}</strong>`;
+  });
 });
 
 submit.addEventListener("click", () => {
   guessNumber();
 });
 
-const restartGame = () => {
-  randomNumber = getRandomNumber(501);
-  console.log(randomNumber);
-  count = 0;
-  score.textContent = "";
-  displayRespons.textContent = "";
-  responsText.textContent = "";
-  restart.style.display = "none";
-};
-
 const guessNumber = () => {
   const subtmitHandler = () => {
+    // console.log(randomNumber);
     const numberChoice = Number(inputNumber.value);
-    console.log(numberChoice);
-    if (!isValidNumber(numberChoice) || !numberChoice) {
+    // console.log(numberChoice);
+    if (!isValidNumber(numberChoice, startNumber, endNumber) || !numberChoice) {
       responsText.style.color = "red";
       responsText.innerHTML = `
-      <p>Veuillez choisir un chiffre entre 1 et 500</p>`;
+      <p>Choisis un nombre entre ${startNumber} et ${endNumber}</p>`;
       inputNumber.value = "";
       return;
     }
@@ -56,42 +98,52 @@ const guessNumber = () => {
       count++;
       responsText.style.color = "black";
       responsText.innerHTML = `
-      <p>Veuillez choisir un nombre plus petit que ${numberChoice}</p>`;
+      <p>Choisis un nombre plus petit que ${numberChoice}</p>`;
       inputNumber.value = "";
-      displayRespons.textContent = "x";
-      displayRespons.style.color = "red";
-      displayRespons.style.left = `${numberChoice}px`;
     }
 
     if (randomNumber > numberChoice) {
       count++;
       responsText.style.color = "black";
       responsText.innerHTML = `
-      <p>Veuillez choisir un nombre plus grand que ${numberChoice}</p>`;
+      <p>Choisis un nombre plus grand que ${numberChoice}</p>`;
       inputNumber.value = "";
-      displayRespons.textContent = "x";
-      displayRespons.style.color = "red";
-      displayRespons.style.left = `${numberChoice}px`;
     }
 
     if (randomNumber === numberChoice) {
       count++;
       responsText.style.color = "green";
       responsText.innerHTML = `
-    
-    <p> Bravo vous avez trouvé le chiffre en ${count} coups !</p>
-    `;
+      
+      <p> Bravo tu as trouvé le chiffre en ${count} coups !</p>
+      `;
       inputNumber.value = "";
-      displayRespons.style.color = "green";
-      displayRespons.textContent = "x";
-      displayRespons.style.left = `${numberChoice}px`;
 
       restart.style.display = "block";
     }
     score.textContent = `Score : ${count} coup(s)`;
   };
-
   subtmitHandler();
+};
+const restartGame = () => {
+  levelContainer.style.display = "grid";
+  levelContainer.style.alignItems = "center";
+  levelContainer.style.justifyItems = "center";
+  levelContainer.style.justifyContent = "center";
+  levelContainer.style.alignContent = "center";
+  levelContainer.style.position = "absolute";
+  levelContainer.style.left = "50%";
+  levelContainer.style.transform = "translate(-50%)";
+  levelContainer.style.bottom = "30%";
+  levelContainer.style.height = "400px";
+  levelContainer.style.width = "400px";
+  levelContainer.style.background = "#d6dee7";
+  levelContainer.style.zIndex = 2;
+
+  startGame.style.display = "none";
+  score.textContent = `Score : `;
+  responsText.textContent = "";
+  restart.style.display = "none";
 };
 
 restart.addEventListener("click", restartGame);
